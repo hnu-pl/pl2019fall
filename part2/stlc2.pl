6970cf9a0fe541c3be6f85a7c9d730a81df1466b
Ki Yung Ahn <kyagrd@gmail.com>
2019-10-10 14:25:50 +0900

    Tau-Prolog error

--- part2/stlc2.pl
+++ part2/stlc2.pl
@@ -15,7 +15,7 @@ synerr(XS) :- err(['SYNTAX ERROR: ' | XS]).
 
 lookup(X, [X:A | _], B) :- !, A=B.
 lookup(X, [_ | XS],  B) :- lookup(X, XS, B).
-lookup(X, [],        _) :- !, err([X,' is not defeind']).
+lookup(X, [],        _) :- !, err([X,' is not defined']).
 
 tc(_, val(_),     int   ).
 tc(G, var(X),     A     ) :- lookup(X, G, A).
@@ -27,3 +27,14 @@ tc(G, app(E1,E2), B     ) :- tc(G, E1, T1), tc(G, E2, T2),
 
 tychk(G,E,T) :- tm(E) -> tc(G,E,T) ; synerr([E,' is not a well-formed term']).
 
+
+% some obvious ill-typed terms succeeds type checking in Tau-Prolog :(.
+%
+% In SWI-Prolog and GNU Prolog, ... this query prints a type error and fails
+%
+% ?- tychk([],app(lam(f,lam(x,app(var(f),var(x)))),val(3)),T).
+% TYPE ERROR: lam(f,lam(x,app(var(f),var(x)))):((_61->_62)->_61->_62) cannot be applied to val(3):int
+% false.
+% 
+% However, in Tau-Prolog as of now 2019-10-10 KST 14:25 it magically succeeds @.@
+%
